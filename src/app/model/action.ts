1e2e999c9774f7691de0300403ae3042f71a5334
Kohe <kohe@localhost.localdomain>
2017-11-20 21:58:41 +0900

    アクションクラスのプロパティを変更

--- src/app/model/action.ts
+++ src/app/model/action.ts
@@ -1,7 +1,15 @@
 import { BATTLE_ACTION } from '../app.const';
 
 export class Action{
+    // バトルフェイズ
     phaze:string = BATTLE_ACTION.PHAZE.PLAYER_TURN;
+    
+    // 主体
+    subject:string = "";
+    
+    // 対象
     target:string = "";
+
+    // ダメージ
     damage:number = 0;
 }
