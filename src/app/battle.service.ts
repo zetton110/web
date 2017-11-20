fc32664bc75e82b544361ce5a9f58bc9903e4ad1
Kohe <kohe@localhost.localdomain>
2017-11-20 21:18:27 +0900

    battle.serviceにコメントを追加

--- src/app/battle.service.ts
+++ src/app/battle.service.ts
@@ -5,30 +5,64 @@ import { Action } from './model/action';
 
 @Injectable()
 export class BattleService{
+
+    //　バトルアクション
     battleAction:Action = new Action(); 
+    
+    // バトルアクションストリーム
     battleAction$ = new BehaviorSubject<Action>(this.battleAction);
     
+    
+    /**
+     * バトルアクションストリームの取得
+     */
     getBattleActionObs(){
         return this.battleAction$.asObservable();
     }
+    
+    /**
+     * 攻撃対象の敵選択開始Actionの生成
+     */
     targetSelect(){
         let action = new Action();
         action.phaze = BATTLE_ACTION.PHAZE.TARGET_SELECT;
         this.battleAction = action;
         this.battleAction$.next(this.battleAction);
     }
+
+    
+    /**
+     * 攻撃対象の敵選択完了Actionの生成
+     */
     targetSelectComplete(){
         let action = new Action();
         action.phaze = BATTLE_ACTION.PHAZE.PLAYER_TURN;
         this.battleAction = action;
         this.battleAction$.next(this.battleAction);
     }
-    attackFromPlayerToEnemy(name:string){
+    
+    /**
+     * プレイヤーから敵へ攻撃する際のActionを生成
+     * 
+     * @param  {string} targetName
+     */
+    attackFromPlayerToEnemy(targetName:string){
         let action = new Action();
         action.phaze = BATTLE_ACTION.PHAZE.PLAYER_TURN;
-        action.target = name;
+        action.target = targetName;
         action.damage = 2;
         this.battleAction = action;
         this.battleAction$.next(this.battleAction);
+    }
+    
+    /**
+     * 敵からプレイヤーへ攻撃する際のActionを生成
+     * 
+     * @param  {string} enemyName
+     */
+    attackFromEnemyToPlayer(enemyName:string){
+        let action = new Action();
+        action.phaze = BATTLE_ACTION.PHAZE.ENEMY_TURN;
+        // TODO: 敵の行動を入れる
     }
 }
