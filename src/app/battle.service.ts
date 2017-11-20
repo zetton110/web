f904431d42a0e967b652e637499f3dfea517a09e
Kohe <kohe@localhost.localdomain>
2017-11-20 21:56:16 +0900

    バトルサービスを修正

--- src/app/battle.service.ts
+++ src/app/battle.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/Rx';
 import { BATTLE_ACTION } from './app.const';
+import { MEMBER } from './app.const';
 import { Action } from './model/action';
 
 @Injectable()
@@ -26,6 +27,7 @@ export class BattleService{
     targetSelect(){
         let action = new Action();
         action.phaze = BATTLE_ACTION.PHAZE.TARGET_SELECT;
+        action.subject = BATTLE_ACTION.SUBJECT.PLAYER;
         this.battleAction = action;
         this.battleAction$.next(this.battleAction);
     }
@@ -37,6 +39,7 @@ export class BattleService{
     targetSelectComplete(){
         let action = new Action();
         action.phaze = BATTLE_ACTION.PHAZE.PLAYER_TURN;
+        action.subject = BATTLE_ACTION.SUBJECT.PLAYER;
         this.battleAction = action;
         this.battleAction$.next(this.battleAction);
     }
@@ -44,25 +47,34 @@ export class BattleService{
     /**
      * プレイヤーから敵へ攻撃する際のActionを生成
      * 
-     * @param  {string} targetName
+     * @param  {string} target
      */
-    attackFromPlayerToEnemy(targetName:string){
+    attackFromPlayerTo(target:string){
         let action = new Action();
         action.phaze = BATTLE_ACTION.PHAZE.PLAYER_TURN;
-        action.target = targetName;
+        action.subject = BATTLE_ACTION.SUBJECT.PLAYER;
+        action.target = target;
         action.damage = 2;
         this.battleAction = action;
         this.battleAction$.next(this.battleAction);
+        // デバッグ
+        setTimeout(
+            ()=>{
+                this.attackToPlayerFrom(MEMBER.KANAGAWA.NAME)
+            }
+        ,5000);
     }
     
     /**
      * 敵からプレイヤーへ攻撃する際のActionを生成
      * 
-     * @param  {string} enemyName
+     * @param  {string} subject
      */
-    attackFromEnemyToPlayer(enemyName:string){
+    attackToPlayerFrom(subject:string){
         let action = new Action();
         action.phaze = BATTLE_ACTION.PHAZE.ENEMY_TURN;
-        // TODO: 敵の行動を入れる
+        action.subject = subject;
+        this.battleAction = action;
+        this.battleAction$.next(this.battleAction);
     }
 }
